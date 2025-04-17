83d5ca539b787bc15538f49bcebb75b2abdf9377
Alexander X <alexfomalhaut@gmail.com>
2025-04-18 00:36:39 +0300

    Pool refactor.

--- src/pool.rs
+++ src/pool.rs
@@ -1,16 +1,16 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::HashSet;
 
 use rand::Rng;
 
+use crate::validate;
 use crate::utils::*;
-use crate::schema::Schema;
-use crate::transaction::{Type, Transaction, Group, group_transactions};
+use crate::transaction::{Type, Transaction, Group};
 use crate::block::Block;
-use crate::state::State;
+use crate::schema::Schema;
+use crate::state::{State, OrderCoinsMap};
 
 
 /// Validator pool that keeps requested transactions.
-#[derive(Debug, Clone)]
 pub struct Pool {
     groups: Vec<Group>,
     senders: Vec<U256>,
@@ -32,177 +32,129 @@ impl Pool {
         self.senders.clear();
     }
 
-    /// Add group to waiting transactions.
-    pub fn add_group(&mut self, group: &Group, state: &State, 
-                     sender: &U256) -> UqoinResult<()> {
-        let group_senders = vec![sender.clone(); group.len()];
-        Block::validate_coins(group.transactions(), state, &group_senders)?;
+    /// Add a new group.
+    pub fn add(&mut self, group: &Group, state: &State, 
+               schema: &Schema) -> UqoinResult<()> {
+        let senders = Transaction::calc_senders(group.transactions(), state, 
+                                                schema);
+        validate!(check_same(senders.iter()), TransactionInvalidSender)?;
+        Block::validate_coins(group.transactions(), state, &senders)?;
         self.groups.push(group.clone());
-        self.senders.push(sender.clone());
+        self.senders.push(senders[0].clone());
         Ok(())
     }
 
-    /// Get ready transactions for next block.
-    pub fn prepare<R: Rng>(&self, rng: &mut R, schema: &Schema, state: &State, 
+    /// Update the pool according to the given state. This function 
+    /// recalculates senders, so it may take a while.
+    pub fn update(&mut self, state: &State, schema: &Schema) {
+        let groups = self.groups.clone();
+        self.groups = Vec::new();
+        self.senders = Vec::new();
+        for group in groups.iter() {
+            let _ = self.add(group, state, schema);
+        }
+    }
+
+    /// Prepare transactions and senders for the next block. The pool must be
+    /// updated to the state.
+    pub fn prepare<R: Rng>(&self, rng: &mut R, state: &State, schema: &Schema,
                            validator_key: &U256, groups_max: Option<usize>) -> 
-                           Vec<Transaction> {
-        // Transactions to return
-        let mut transactions = vec![];
+                           (Vec<Transaction>, Vec<U256>) {
+        // Transactions and senders to fill
+        let mut transactions = Vec::new();
+        let mut senders = Vec::new();
 
         // Validator public
         let validator = schema.get_public(validator_key);
 
         // Validator resource
-        let mut validator_resource = if let Some(coins_map) = 
-                state.get_coins(&validator) {
-            coins_map.iter().map(|(order, coins)| 
-                (*order, Vec::<U256>::from_iter(coins.iter().cloned()))
-            ).collect::<HashMap<u64, Vec<U256>>>()
-        } else {
-            HashMap::new()
-        };
+        let mut validator_resource = state.get_coins(&validator).cloned()
+                                          .unwrap_or(OrderCoinsMap::new());
 
-        // Set for seen coins to avoid repeating
-        let mut coin_set = HashSet::new();
+        // Set of seen coins
+        let mut coins_seen = HashSet::new();
 
-        // Look for all groups
-        let groups_max = groups_max.unwrap_or(1000000000);
-        for (group, sender) in self.groups.iter().zip(self.senders.iter())
-                                          .take(groups_max) {
-            // Skip if coins are invalid
-            let group_senders = vec![sender.clone(); group.len()];
-            if Block::validate_coins(group.transactions(), state, 
-                                     &group_senders).is_err() {
-                continue;
+        // Counter of added groups
+        let mut counter = 0;
+
+        // Loop for groups and corresponding senders
+        for (group, sender) in self.groups.iter().zip(self.senders.iter()) {
+            // Leave if groups_max is reached
+            if let Some(groups_max) = groups_max {
+                if counter >= groups_max {
+                    break;
+                }
             }
-
-            // Get order
-            let order = group.get_order(state, 
-                                        &vec![sender.clone(); group.len()]);
 
             // Skip if the group contains any seen coin
             if group.transactions().iter()
-                    .any(|tr| coin_set.contains(&tr.coin)) {
+                    .any(|tr| coins_seen.contains(&tr.coin)) {
                 continue;
             }
 
             // Update seen coins
             for tr in group.transactions().iter() {
-                coin_set.insert(tr.coin.clone());
+                coins_seen.insert(tr.coin.clone());
             }
+
+            // Group senders
+            let group_senders = vec![sender.clone(); group.len()];
+
+            // Get order
+            let order = group.get_order(state, &group_senders);
 
             // Calculate ext transactions
             let ext_trs: Option<Vec<Transaction>> = match group.get_type() {
                 Type::Transfer => Some(vec![]),
                 Type::Merge => [order].iter().map(|ord| {
                     let coin = Self::get_validator_coin(
-                        ord, &mut validator_resource, &coin_set
+                        ord, &mut validator_resource, &coins_seen
                     )?;
                     let counter = state.get_coin_counter(&coin);
-                    coin_set.insert(coin.clone());
+                    coins_seen.insert(coin.clone());
                     Some(Transaction::build(rng, coin, sender.clone(), 
                                             validator_key, counter, schema))
                 }).collect(),
                 Type::Split => [order-1, order-2, order-2].iter().map(|ord| {
                     let coin = Self::get_validator_coin(
-                        ord, &mut validator_resource, &coin_set
+                        ord, &mut validator_resource, &coins_seen
                     )?;
                     let counter = state.get_coin_counter(&coin);
-                    coin_set.insert(coin.clone());
+                    coins_seen.insert(coin.clone());
                     Some(Transaction::build(rng, coin, sender.clone(), 
                                             validator_key, counter, schema))
                 }).collect(),
                 _ => panic!("Invalid group type"),
             };
 
-            // Extend transactions if ext was added
+            // Extend transactions and senders if ext was added
             if let Some(ext_trs) = ext_trs {
+                senders.extend(group_senders);
+                senders.extend(vec![validator.clone(); ext_trs.len()]);
+
                 transactions.extend(group.transactions().iter().cloned());
                 transactions.extend(ext_trs);
+
+                counter += 1;
             }
         }
 
-        // Return collected transactions
-        transactions
+        // Return transactions and senders
+        (transactions, senders)
     }
 
     /// Pop coin from the resource by order ignoring specified coins.
-    fn get_validator_coin(order: &u64, resource: &mut HashMap<u64, Vec<U256>>, 
+    fn get_validator_coin(order: &u64, resource: &mut OrderCoinsMap, 
                           ignore_coins: &HashSet<U256>) -> Option<U256> {
-        if let Some(series) = resource.get_mut(&order) {
-            while let Some(coin) = series.pop() {
-                if !ignore_coins.contains(&coin) {
-                    return Some(coin);
-                }
+        if let Some(set) = resource.get_mut(&order) {
+            let coin_opt = set.iter().filter(|c| !ignore_coins.contains(c))
+                              .next().cloned();
+
+            if let Some(coin) = coin_opt {
+                set.remove(&coin);
+                return Some(coin);
             }
         }
         None
-    }
-
-    /// Update the pool with transactions of the new block. `state` is supposed 
-    /// to be already rolled up.
-    pub fn roll_up(&mut self, transactions: &[Transaction], state: &State) {
-        let coins = transactions.iter().map(|tr| tr.coin.clone())
-            .collect::<HashSet<U256>>();
-
-        let mut groups = Vec::new();
-        let mut senders = Vec::new();
-
-        for (group, sender) in self.groups.iter().zip(self.senders.iter()) {
-            let group_senders = vec![sender.clone(); group.len()];
-            let coins_are_not_repeated = group.transactions().iter()
-                .all(|tr| !coins.contains(&tr.coin));
-            let coins_are_valid = Block::validate_coins(
-                group.transactions(), state, &group_senders
-            ).is_ok();
-            if coins_are_not_repeated && coins_are_valid {
-                groups.push(group.clone());
-                senders.push(sender.clone());
-            }
-        }
-
-        self.groups = groups;
-        self.senders = senders;
-    }
-
-    /// Roll back the pool state with transactions of the last block. `state` 
-    /// is supposed to be already rolled down.
-    pub fn roll_down(&mut self, transactions: &[Transaction], state: &State, 
-                     senders: &[U256]) {
-        for (ofs, gr, _) in group_transactions(transactions.to_vec(), state, 
-                                               senders) {
-            let _ = self.add_group(&gr, state, &senders[ofs]);
-        }
-    }
-
-    /// Remove invalid groups according to the given state.
-    pub fn update(&mut self, state: &State) {
-        let mut groups = Vec::new();
-        let mut senders = Vec::new();
-
-        for (group, sender) in self.groups.iter().zip(self.senders.iter()) {
-            let group_senders = vec![sender.clone(); group.len()];
-            if Block::validate_coins(group.transactions(), state, 
-                                     &group_senders).is_ok() {
-                groups.push(group.clone());
-                senders.push(sender.clone());
-            }
-        }
-
-        self.groups = groups;
-        self.senders = senders;
-    }
-
-    /// Merge pools, the state must correspond to `other` pool.
-    pub fn merge(&mut self, other: &Self, state: &State, schema: &Schema) {
-        for group in other.groups.iter() {
-            let senders = Transaction::calc_senders(
-                &group.transactions(), state, schema
-            );
-            if check_same(senders.iter()) {
-                let _ = self.add_group(&group, state, &senders[0]);
-            }
-        }
-        self.update(state);
     }
 }
